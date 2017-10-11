91bbdc92d5752b05a143e082653b94f4dd99df9b
= <ekirapapaul@gmail.com>
2017-10-11 11:12:26 +0300

    update token

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class auth_config:
     """ Auth config class"""
     def get_auth_token(self):
         """ retutn auth token """
-        token = 'f8452af03fee45dba004754aecc463a34d291042'
+        token = '952c3dbad9bbef5f75aa5d137bf0ba291ace5f4c'
         return token
 
     def json_content_type(self):
