f1da7edc3238b866a90b5eaed0abd62d82d9d5c4
= <ekirapapaul@gmail.com>
2017-11-06 15:43:50 +0300

    added message utils

---
+++ messageutils.py
@@ -0,0 +1,5 @@
+#File with methods to build messages
+
+def enrollment_message(name, program):
+    """ Return enrrollment name """
+    return 'Welcome {} to the Wellness {} program'.format(name, program)
