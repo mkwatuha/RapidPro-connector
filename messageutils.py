82f397d625da0f626deca962077e7e28ee09bb0a
= <ekirapapaul@gmail.com>
2017-11-08 13:05:44 +0300

    added messages to essage utils

--- messageutils.py
+++ messageutils.py
@@ -3,3 +3,23 @@
 def enrollment_message(name, program):
     """ Return enrrollment name """
     return 'Welcome {} to the Wellness {} program'.format(name, program)
+
+def program_kick_off_message(name):
+    """ Return kickoff message"""
+    return 'Dear {}, today marks your wellness journey. The secret of getting ahead is getting started. We will wall with you through the journey.'.format(name)
+
+def goal_congratulatory_message(name):
+    """ Return congratulatory message """
+    return 'Dear {} congratulations on reaching your goal weight, kindly remember to get in touch with your wellness partner for further guidelines.'.format(name)
+
+def completion_message(name):
+    """ Return completion cogratulatory message """
+    return 'Dear {}, congratulations on completing your wellness journey, this is the begining of a healthy, happy, holistic you. I f you wish to share your story kindly contacct your wellness partner.'.format(name)
+
+def birthday_message(name):
+    """ Return birthday message """
+    return 'Life should not only be lived it should be celebrated. Happy birthday {}.'.format(name)
+
+def appointment_booking_message(client_name, partner_name, date, time, venue):
+    """ Return appointment booking message alert """
+    return 'Dear {}, your appointment has been confirmed with {} on {} at {} at {}'.format(client_name, partner_name, date, time, venue)
